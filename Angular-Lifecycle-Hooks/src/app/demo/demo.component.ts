82c58219f0b7568d6ba11549fe171c088b34b112
siyam prasanth <prshiyam@gmail.com>
2023-11-06 20:16:24 +0530

    06-11-2023(later)

--- Angular-Lifecycle-Hooks/src/app/demo/demo.component.ts
+++ Angular-Lifecycle-Hooks/src/app/demo/demo.component.ts
@@ -1,27 +1,39 @@
-import { Component,Input,OnChanges,SimpleChanges } from '@angular/core';
+import { Component,Input,OnChanges,SimpleChanges,OnInit, ViewChild, ElementRef,DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.css']
 })
-export class DemoComponent implements OnChanges {
+export class DemoComponent implements OnChanges, OnInit, DoCheck {
 title: string = 'DemoComponent' ;
 
 
   @Input()
   message!: string;
+  
+  @ViewChild('temp')
+  tempPara!: ElementRef;
 
 constructor (){
   
   console.log('demo component constructor called');
-  console.log(this.title);
-  console.log(this.message);
+  // console.log(this.title);
+  // console.log(this.message);
   
 }
 ngOnChanges(Changes:SimpleChanges){
   console.log('ngOnChanges hook called');
-  console.log(Changes);
+  // console.log(Changes);
+  
+}
+ngOnInit(){
+  console.log('ngOnInit hook Called');
+  // console.log(this.tempPara.nativeElement.innerHTML);
+  
+}
+ngDoCheck(){
+  console.log('ngDOCheck hook called');
   
 }
 }
